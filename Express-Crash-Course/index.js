77b410e155782af2f778728bc7e2359d26325a40
Edson Cepeda <raulcepedac@hotmail.com>
2020-03-31 17:09:44 -0600

    mysql added

--- Express-Crash-Course/index.js
+++ Express-Crash-Course/index.js
@@ -3,10 +3,58 @@ const path = require('path');
 const logger = require('./middleware/logger');
 const exphbs = require('express-handlebars');
 const members = require('./Members');
+const mysql = require('mysql');
 const app = express();
 
 // Init middleware
 // app.use(logger);
+
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if(err) {
+        throw err;
+    } 
+    console.log('MySQL connected');
+});
+
+// Create DB
+app.get('/createdb', (req,res) => {
+   let sql = 'CREATE DATABASE nodemysql' ;
+   db.query(sql, (err,result) => {
+      if(err) throw err;
+      res.send('Database created');
+      console.log(result);
+   });
+});
+
+// create table
+app.get('/createpoststable',(req,res) => {
+   let sql= 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+   db.query(sql, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Posts table created...');
+   });
+});
+
+//Insert post 1
+app.get('/addpost1', (req,res) => {
+    let post = {title: 'Post1', body:'This is post number one'};
+    let sql = 'INSERT INTO posts SET ? ';
+    let query = db.query(sql,post,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post one added');
+    });
+});
 
 // Handlebars middleware
 app.engine('handlebars',exphbs({defaultLayout: 'main'}));
