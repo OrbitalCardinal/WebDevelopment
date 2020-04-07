d7a2dfaa68c17b25218ebe232d9b1712dfea306b
Edson Cepeda <raulcepedac@hotmail.com>
2020-04-06 20:45:17 -0500

    added edit posts

--- MEAN-Stack-Course/mean-course/backend/app.js
+++ MEAN-Stack-Course/mean-course/backend/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
-
+const postsRoutes = require('./routes/posts');
 const app = express();
 
 mongoose.connect("mongodb+srv://Orbital:PEPBOIqraE5fJXLr@cluster0-mqirq.mongodb.net/node-angular?retryWrites=true&w=majority",
@@ -24,37 +23,10 @@ app.use((req,res,next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
     );
-  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   next();
 } );
-app.post("/api/posts", (req,res,next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'post added successfully',
-      postId: createdPost._id
-    });
-  });
 
-});
-
-app.get('/api/posts',(req, res, next) => {
-  Post.find().then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched succesfully',
-        posts: documents
-      });
-    });
-});
-
-app.delete("/api/posts/:id", (req,res,next) => {
-   Post.deleteOne({_id: req.params.id}).then(result => {
-     console.log(result);
-     res.status(200).json({message: "Post Deleted"});
-   })
-});
+app.use("/api/posts",postsRoutes);
 
 module.exports = app;
--- MEAN-Stack-Course/mean-course/src/app/posts/post-create/post-create.component.ts
+++ MEAN-Stack-Course/mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
-import  { Component, EventEmitter, Output } from '@angular/core';
+import  { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { from } from 'rxjs';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
@@ -10,21 +11,43 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-create.component.css']
 })
 
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
+  post: Post;
+  isLoading = false;
+  private mode = 'create';
+  private postId: string;
+
   // CONVENTION: You always start with 'on' methods that are triggered with events
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
-  onAddPost(form: NgForm) {
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if(paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if(form.invalid) return;
-    const post: Post = {
-      id: null,
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postsService.addPost(form.value.title, form.value.content);
+    this.isLoading = true;
+    if(this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId,form.value.title, form.value.content);
+    }
     form.resetForm();
   }
 }
