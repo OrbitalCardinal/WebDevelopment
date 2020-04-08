e530787fb88d630565fc00125a9ba35ee56ee727
Edson Cepeda <raulcepedac@hotmail.com>
2020-04-07 22:21:37 -0500

    Added image upload

--- MEAN-Stack-Course/mean-course/backend/app.js
+++ MEAN-Stack-Course/mean-course/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const path = require("path");
 const postsRoutes = require('./routes/posts');
 const app = express();
 
@@ -16,6 +16,7 @@ mongoose.connect("mongodb+srv://Orbital:PEPBOIqraE5fJXLr@cluster0-mqirq.mongodb.
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})); //Not necessary
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
