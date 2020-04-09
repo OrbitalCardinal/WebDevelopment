c444b108b93029757fd7d2236c8f6a2b8096c108
Edson Cepeda <raulcepedac@hotmail.com>
2020-04-09 18:30:10 -0500

    added pagination

--- MEAN-Stack-Course/mean-course/backend/routes/posts.js
+++ MEAN-Stack-Course/mean-course/backend/routes/posts.js
@@ -63,10 +63,23 @@ router.put("/:id",multer({storage: storage}).single("image"), (req,res,next) => 
 })
 
 router.get("",(req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = req.query.pagesize;
+  const currentPage = req.query.page;
+  const postQuery = Post.find();
+  let fetchedPost;
+  if(pageSize && currentPage) {
+    postQuery
+      .skip(pageSize & (currentPage - 1))
+      .limit(parseInt(pageSize));
+  }
+  postQuery.find().then(documents => {
+    fetchedPost = documents;
+      return Post.count();
+    }).then(count => {
       res.status(200).json({
         message: 'Posts fetched succesfully',
-        posts: documents
+        posts: fetchedPost,
+        maxPosts: count
       });
     });
 });
--- MEAN-Stack-Course/mean-course/src/app/app.module.ts
+++ MEAN-Stack-Course/mean-course/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatPaginatorModule } from '@angular/material/paginator'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
@@ -36,7 +38,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatToolbarModule,
     MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
