c9b2b3225e3d5e74f834200da5dc74226a5e319c
Edson Cepeda <raulcepedac@hotmail.com>
2020-06-30 17:13:22 -0500

    Finished homework 3

--- Angular-Full-Course/Homeworks/basics-assignment-3-start/src/app/app.component.ts
+++ Angular-Full-Course/Homeworks/basics-assignment-3-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  enable: boolean = false;
+  clickLog: number[] = [];
+  counter: number = 1;
+  color: String = 'white';
+  enableText() {
+    this.enable = !this.enable;
+    this.clickLog.push(this.counter);
+    this.counter = this.counter + 1;
+
+
+  }
 }
